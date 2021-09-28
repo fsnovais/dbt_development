e825fa5691d444355c4122cbdf918b92f3d69d51
Felipe Novais <felipe.novais@brfconsulting.com>
2021-09-28 22:34:29 +0000

    updated tests, docs and sources

--- models/staging/jaffle_Shop/stg_customers.sql
+++ models/staging/jaffle_Shop/stg_customers.sql
@@ -1,11 +1,5 @@
-with customers as (
-
-    select
-        id as customer_id,
-        first_name,
-        last_name
-
-    from dev.jaffle_shop.customers
-
-)
-select * from customers
+select 
+    id as customer_id,
+    first_name,
+    last_name
+from {{ source('jaffle_shop', 'customers') }}
